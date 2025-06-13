8e978390e1fcab4437e283b31803340c5bfe9566
devrisby <118779687+devrisby@users.noreply.github.com>
2025-06-13 15:01:41 -0400

    adds first challenge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Daily Svelte</h1>
+    <p>Source <a href="https://github.com/rezbee-dev/svelte-everydays">repository</a></p>
+
+    <section>
+        <h2>Challenges:</h2>
+        <ul>
+            <li><a href="/challenges/counter">Counter</a></li>
+        </ul>
+    </section>
+</main>
+
+<style>
+    main {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        /* justify-content: center; */
+        align-items: center;
+        margin-top: 12px;
+    }
+
+    h2 {
+        margin-bottom: 0;
+    }
+
+    ul {
+        margin-top: 0;
+    }
+</style>
---
+++ src/routes/challenges/counter/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    let value = $state(0)
+    function onclick() {
+        value += 1
+    }
+</script>
+
+<main>
+    <section>
+        <input type="number" {value} readonly/>
+        <button {onclick}>Count</button>
+    </section>
+</main>
+
+<style>
+    main {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input {
+        /* width: 140px; */
+        text-align: right;
+        padding: 0;
+    }
+</style>
