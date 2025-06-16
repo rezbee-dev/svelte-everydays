e5438bf65951f0efa954f90717296f3662303685
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2025-06-15 21:58:19 -0400

    adds table for displaying challenges

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import { base } from '$app/paths';
+    import Table from '$lib/Table.svelte';
+    import { challenges } from './challenges';
 </script>
 
 <main>
@@ -7,16 +9,18 @@
     <p>Source <a href="https://github.com/rezbee-dev/svelte-everydays">repository</a></p>
 
     <section>
-        <h2>Challenges:</h2>
+        <!-- <h2>Challenges:</h2>
         <ul>
             <li><a href="{base}/challenges/counter">Counter</a></li>
-        </ul>
+        </ul> -->
+        <Table { challenges } />
     </section>
 </main>
 
 <style>
     main {
         min-height: 100vh;
+        width: 100%;
         display: flex;
         flex-direction: column;
         /* justify-content: center; */
@@ -30,5 +34,9 @@
 
     ul {
         margin-top: 0;
+    }
+
+    section {
+        width: 80%;
     }
 </style>
---
+++ src/routes/challenges.js
@@ -0,0 +1,23 @@
+export const challenges = [
+    {
+        "date":"June 12, 2025",
+        "challenge":"Build a counter",
+        "challengeLink": "challenges/counter",
+        "sourceURL":"https://eugenkiss.github.io/7guis/tasks/",
+        "sourceLabel":"7 GUIs Tasks"
+    },
+    {
+        "date":"June 13, 2025",
+        "challenge":"Build a counter",
+        "challengeLink": "challenges/counter",
+        "sourceURL":"https://eugenkiss.github.io/7guis/tasks/",
+        "sourceLabel":"7 GUIs Tasks"
+    },
+    {
+        "date":"June 14, 2025",
+        "challenge":"Build a counter",
+        "challengeLink": "challenges/counter",
+        "sourceURL":"https://eugenkiss.github.io/7guis/tasks/",
+        "sourceLabel":"7 GUIs Tasks"
+    },
+]
